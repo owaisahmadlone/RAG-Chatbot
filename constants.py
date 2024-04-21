edeb5fcd592202ff46b654c0ad3c8da494f3a45a
Owais Ahmad Lone <130448559+owaisahmadlone@users.noreply.github.com>
2024-04-21 11:29:53 +0000

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Constants
+EMB_INSTRUCTOR_XL = "hkunlp/instructor-xl"
+EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" # Chroma takes care if embeddings are None
+EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2" # Chroma takes care if embeddings are None
+
+
+LLM_FLAN_T5_XXL = "google/flan-t5-xxl"
+LLM_FLAN_T5_XL = "google/flan-t5-xl"
+LLM_FASTCHAT_T5_XL = "lmsys/fastchat-t5-3b-v1.0"
+LLM_FLAN_T5_SMALL = "google/flan-t5-small"
+LLM_FLAN_T5_BASE = "google/flan-t5-base"
+LLM_FLAN_T5_LARGE = "google/flan-t5-large"
